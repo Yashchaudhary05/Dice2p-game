cad4a2044cf7f6a85e835f65624ea4d63a714870
Yash Chaudhary <137133168+Yashchaudhary05@users.noreply.github.com>
2024-03-06 19:14:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1= Math.random();
+randomNumber1=Math.floor(randomNumber1*6) + 1;
+document.querySelectorAll("img")[0].setAttribute("src","images/dice"+randomNumber1+".png");
+
+
+var randomNumber2 = Math.random();
+randomNumber2 = Math.floor(randomNumber2*6)+1;
+document.querySelectorAll("img")[1].setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+if (randomNumber1 > randomNumber2)
+{
+    document.querySelector("h1").textContent="🚩 Player 1 Wins!";
+} 
+else if (randomNumber1 < randomNumber2)
+{
+    document.querySelector("h1").textContent="Player 2 Wins! 🚩 ";
+}
+else
+{
+    document.querySelector("h1").textContent=" DRAW !!";
+}
